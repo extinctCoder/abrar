44e09907ab5000f01a691f4bd2fece1a59a96197
darthVader <extinctCoder@outlook.com>
2019-08-02 12:09:15 +0600

    file1.cpp

---
+++ aug2-2019/file1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int choice = 0;
+
+    int id[3];
+    string name[3];
+    string uni[3];
+
+    cout << "please enter your id : ";
+    cin >> id[choice];
+
+    cout << "please enter your name : ";
+    cin >> name[choice];
+
+    cout << "please enter your uni : ";
+    cin >> uni[choice];
+
+    cout << "your id is : " << id[choice] << endl;
+    cout << "your name is : " << name[choice] << endl;
+    cout << "your uni is : " << uni[choice] << endl;
+    
+    return 0;
+}
